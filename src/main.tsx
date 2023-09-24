4ae979e91af58d3daf10f76f7e59ade16403d65c
perako2023 <135764815+perako2023@users.noreply.github.com>
2023-09-24 11:15:59 -0500

    chore: add next-themes pkg

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './globals.css'
 import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider } from 'next-themes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<NextUIProvider>
-			<App />
+			<ThemeProvider attribute="class" defaultTheme="dark">
+				<App />
+			</ThemeProvider>
 		</NextUIProvider>
 	</React.StrictMode>
 )
